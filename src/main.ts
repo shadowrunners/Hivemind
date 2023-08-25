63b8adbd155a6d3fef657a9078670ddd833e0335
scrappie <scr3ppie@gmail.com>
2023-08-25 13:08:47 +0300

    refactor(main): proxied all routes under /api

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,26 @@
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './api/app.module';
+import { AppModule } from './app.module';
 import { WEB_URL } from '@/config';
 import { connect } from 'mongoose';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    credentials: true,
-    maxAge: 40,
-    origin: WEB_URL,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'HEAD', 'POST', 'DELETE', 'PATCH'],
-  });
+	const app = await NestFactory.create(AppModule);
 
-  connect(process.env.DATABASE_URL)
+	app.setGlobalPrefix('/api');
+
+	app.enableCors({
+		credentials: true,
+		maxAge: 40,
+		origin: WEB_URL,
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		methods: ['GET', 'HEAD', 'POST', 'DELETE', 'PATCH'],
+	});
+
+	connect(process.env.DATABASE_URL)
 	  .then(() => console.log('Archie has connected to the database.'))
 	  .catch((err) => console.log(err));
 
-  await app.listen(process.env.PORT ?? 8080);
+	await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
