65d2a3de5f63016570010713e7cce86bb887f397
scrappie <scr3ppie@gmail.com>
2023-08-25 14:09:29 +0300

    refactor(utils): removed decrypt function

--- src/utils/secureStorage.ts
+++ src/utils/secureStorage.ts
@@ -6,7 +6,6 @@ export class SecureStorage {
 	 * Encrypts the provided value.
 	 * @public
 	 * @param value The value that will be encrypted.
-	 * @param client The Evelyn object.
 	 * @returns {string}
 	 */
 	public encrypt(value: string): string {
@@ -16,21 +15,5 @@ export class SecureStorage {
 		});
 
 		return keepMyDataSecure.encrypt(value);
-	}
-
-	/**
-	 * Decrypts the provided value.
-	 * @public
-	 * @param encryptedValue The value that will be decrypted.
-	 * @param client The Evelyn object.
-	 * @returns {string}
-	 */
-	public decrypt(encryptedValue: string): string {
-		const keepMyDataSecure = new cryptr(process.env.DECRYPTION_KEY, {
-			pbkdf2Iterations: 15000,
-			saltLength: 15,
-		});
-
-		return keepMyDataSecure.decrypt(encryptedValue);
 	}
 }
