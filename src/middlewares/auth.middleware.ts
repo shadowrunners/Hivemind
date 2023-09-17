78476948aec44a722ef510854da81456dcca54e0
scrappie <notscrappie@proton.me>
2023-09-17 23:24:08 +0300

    fix(auth): fixed authentication (again)

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,27 +1,12 @@
 import { HttpStatus, HttpException, Injectable, NestMiddleware } from '@nestjs/common';
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { UserSession } from '@/utils/discord';
-
-function getToken(req: FastifyRequest): UserSession {
-	const data = req.headers.authorization as string | null;
-
-	if (!data || !data.startsWith('Bearer '))
-		throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
-
-	return {
-		token_type: 'Bearer',
-		access_token: data.slice('Bearer'.length).trim(),
-	};
-}
-
-export interface AuthRequest {
-  session: UserSession;
-}
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-	use(req: FastifyRequest, _: FastifyReply['raw'], next: () => void) {
-		req.session = getToken(req);
+	use(req: FastifyRequest['raw'], _: FastifyReply['raw'], next: () => void) {
+		if (!req.headers.authorization || !req.headers.authorization?.startsWith('Bearer '))
+			throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+
 		next();
 	}
 }
