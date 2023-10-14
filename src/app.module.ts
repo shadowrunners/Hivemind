fee1a6432363f74bf736956557abfb6a8abfa135
scrappie <notscrappie@proton.me>
2023-10-15 01:18:18 +0300

    refactor: removed prisma + services from appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
-import { BotService } from './services/bot.service';
-import { PrismaService } from './services/prisma.service';
-
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { GuildController } from './controllers/guild.controller';
+import { GuildController } from './guilds/controllers/guild.controller';
 import { AuthMiddleware } from './middlewares/auth.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GuildsModule } from './guilds/guild.module';
 
 @Module({
-	controllers: [GuildController],
-	providers: [BotService, PrismaService],
+	imports: [MongooseModule.forRoot(process.env.DATABASE_URL), GuildsModule],
 })
 export class AppModule implements NestModule {
 	configure(consumer: MiddlewareConsumer) {
